567dd7446a63e008dc247696bfee72c4878110ba
jamesjo <jamescakr@gmail.com>
2025-03-20 22:25:33 -0600

    changed description

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import Box from "./component/Box";
 import React, { Component } from "react";
 import BackgroundMusic from "./component/BackgroundMusic";
 
-//가위바위보 >> 카드게임으로 전환 플랜
-//1. 가위:궁수, 바위:투석기, 보:전사
-//1-1 궁수(가위): 장거리 공격이 가능하여 멀리서 오는 전사를 쉽게 처리할수있음, 기동력이 낮아 투석기 공격에 취약
-//1-2 투석기(바위): 공격력이 높고 장거리 공격이 가능하여 기동력이 낮은 궁수를 쉽게 처리할수있음, 이동이 불가하여 기동력이 높은 전사에게 취약
-//1-3 전사(보): 기동력이 높아 투석기 공격을 회피할수있고 쉽게 접근가능하여 투석기를 쉽게 처리할수있음, 근접공격만 가능하여 궁수에게 취약
-//2. 위쳐3의 궨트 카드게임을 응용 - 궨트 카드로 이미지파일
-//3. bgm + 효과음 넣기
-//4. CSS 애니메이션을 통해 효과 주기 (클릭후 애니메이션 재생 >> 3~4초 딜레이후 결과 나타내기)
-
 const choice = {
   archer: {
     name: "Archer",
@@ -134,29 +125,17 @@ class App extends Component {
             className="instruction-background"
           />
           <div className="instruction-text">
-            <div>
-              &lt;<b>가위 궁수</b>&gt;
-            </div>
-            <div>
-              장거리 공격이 가능하여 멀리서 오는 <b>보 전사를 쉽게 제압가능</b> //
-              기동력이 낮아 <b>투석기 공격에 취약</b>
-            </div>
-            <div className="spaced">
-              &lt;<b>바위 투석기</b>&gt;
-              <div>
-                공격력이 높고 장거리 공격이 가능하여 기동력이 낮은
-                <b>가위 궁수를 쉽게 제압가능</b> // 이동이 불가하여 기동력이 높은{" "}
-                <b>보 전사에게 취약</b>
-              </div>
-            </div>
-            <div className="spaced">
-              &lt;<b>보 전사</b>&gt;
-              <div>
-                기동력이 높아 쉽게 접근가능하여
-                <b>투석기를 쉽게 제압가능</b>, 근접공격만 가능하여{" "}
-                <b>가위 궁수에게 취약</b>
-              </div>
-            </div>
+            <ul>
+              <li>
+                &lt;<b>Archer</b>&gt; beats Warrior
+              </li>
+              <li className="spaced">
+                &lt;<b>Warrior</b>&gt; beats Catapult
+              </li>
+              <li className="spaced">
+                &lt;<b>Catapult</b>&gt; beats Archer
+              </li>
+            </ul>
           </div>
         </div>
       </div>
